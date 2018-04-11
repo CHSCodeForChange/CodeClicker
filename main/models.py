90f051e30382599e745dbe4ad5f1980ee0eeeaf9
Armaan Goel <armaangoel78@gmail.com>
2018-04-11 13:39:23 -0400

    added prize feature

--- main/models.py
+++ main/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Code(models.Model):
     objects = models.Manager()
     clicks = models.IntegerField(null=False, blank=False, default=0)
+    prize = models.IntegerField(null=False, blank=False, default=0)
