d7dc9dea04120d0a6b96b5609b98749cce156cac
jpaav <joepaavola@gmail.com>
2018-04-11 12:06:58 -0400

    update update.js

--- static/update.js
+++ static/update.js
@@ -6,10 +6,11 @@ var chatSocket = new WebSocket(
         document.querySelector('#click-count').value = "Clicks: " + message;
     };
     chatSocket.onclose = function(e) {
-        console.error('Chat socket closed unexpectedly');
+        console.error('Socket closed unexpectedly');
     };
 
 document.querySelector('#clicker').onclick = function(e) {
+    console.info('testing')
     chatSocket.send(JSON.stringify({
         'addAmount': 1
     }));
