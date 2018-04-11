2bbe55f543f0e3f26bc035faacffcc104edbc60a
jpaav <joepaavola@gmail.com>
2018-04-11 06:05:55 -0400

    Fixed error if no code object exists

--- main/views.py
+++ main/views.py
@@ -6,7 +6,11 @@ from .models import Code
 
 # Sending user object to the form, to verify which fields to display/remove (depending on group)
 def click(request):
-    code = Code.objects.first()
+    if Code.objects.count() == 0:
+        code = Code.objects.create()
+        # You can do something here as this should be the first person
+    else:
+        code = Code.objects.first()
     code.clicks = code.clicks + 1
     code.save()
     print(code.clicks)
