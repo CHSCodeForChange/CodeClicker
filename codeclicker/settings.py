707aff387b96cdcd15359f66c6f5b704e0075498
jpaav <joepaavola@gmail.com>
2018-04-12 06:52:38 -0400

    Got rid of bad DB name
    Mostly just style, wouldn't have caused any problems most likely

--- codeclicker/settings.py
+++ codeclicker/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'codeclicker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db',
+        'NAME': '',
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
