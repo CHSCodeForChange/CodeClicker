e97e8acd7ed6b8e6ab55ca5a34b1f1a9b6dee92d
Armaan Goel <armaangoel78@gmail.com>
2018-08-06 00:17:57 -0400

    Added leaderboard view

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^click/', views.post_click),
     url(r'^click_data/', views.get_code_data),
     url(r'^user_data/', views.get_user_data),
+    url(r'^leaderboard/', views.get_leaderboard),
 ]
--- api/views.py
+++ api/views.py
@@ -7,7 +7,7 @@ from main.models import Code, User
 from .serializers import CodeSer, UserSer
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def post_click(request):
     name = request.GET.get('name')
     ip = get_client_ip(request)
@@ -28,13 +28,13 @@ def post_click(request):
     return Response()
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def get_code_data(request):
     clicks = Code.objects.all().first()
     serializer = CodeSer(clicks)
     return Response(serializer.data, template_name="api.html")
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def get_user_data(request):
     ip = get_client_ip(request)
     user = User.objects.filter(ip=ip).first()
@@ -44,6 +44,13 @@ def get_user_data(request):
         user.save()
 
     serializer = UserSer(user)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_leaderboard(request):
+    user = User.objects.all().order_by('-clicks')[:5]
+    
+    serializer = UserSer(user, many=True)
     return Response(serializer.data)
 
 def get_client_ip(request):
